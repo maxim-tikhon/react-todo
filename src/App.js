b8406d8f5ce6f8d8ba22fa1eab3a2b9a699afa95
maxim-tikhon <tikhon.maxim@gmail.com>
2023-05-15 22:23:38 +0200

    add page component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
 import "./App.scss";
-import TodoTemplate from "./templates/TodoTemplate/TodoTemplate";
+import TodoPage from "./pages/TodoPage/TodoPage";
 
 function App() {
-	const tasks = [
-		{
-			id: 1,
-			description: "Read React documentation",
-			completed: true,
-		},
-		{
-			id: 2,
-			description: "Complete React course",
-			completed: false,
-		},
-		{
-			id: 3,
-			description: "Learn English",
-			completed: false,
-		},
-		{
-			id: 4,
-			description: "Listen to music",
-			completed: false,
-		},
-		{
-			id: 5,
-			description: "Cook a dinner for family",
-			completed: false,
-		},
-	];
-
 	return (
-		<TodoTemplate tasks={tasks} />
+		<TodoPage />
 	);
 }
 
---
+++ src/pages/TodoPage/TodoPage.js
@@ -0,0 +1,37 @@
+import TodoTemplate from "../../templates/TodoTemplate/TodoTemplate";
+
+const TodoPage = () => {
+  const tasks = [
+		{
+			id: 1,
+			description: "Read React documentation",
+			completed: true,
+		},
+		{
+			id: 2,
+			description: "Complete React course",
+			completed: false,
+		},
+		{
+			id: 3,
+			description: "Learn English",
+			completed: false,
+		},
+		{
+			id: 4,
+			description: "Listen to music",
+			completed: false,
+		},
+		{
+			id: 5,
+			description: "Cook a dinner for family",
+			completed: false,
+		},
+	];
+
+	return (
+		<TodoTemplate tasks={tasks} />
+	);
+};
+
+export default TodoPage;
