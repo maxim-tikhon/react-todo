d0b03639d95f47cefaa9b09d57608808e0b4b04f
maxim-tikhon <tikhon.maxim@gmail.com>
2023-05-17 00:41:25 +0200

    reset input after Enter click

--- src/atoms/TextInput/TextInput.js
+++ src/atoms/TextInput/TextInput.js
@@ -1,15 +1,20 @@
+import { useRef } from "react";
 import styles from "./TextInput.module.scss";
 
 const TextInput = ({ className, onEnter }) => {
+  const inputRef = useRef(null);
+
   const handlePress = e => {
     if (e.key === "Enter") {
       onEnter(e.target.value);
+      inputRef.current.value = "";
     }
   }
 
 	return <input
     type="text"
     className={`${styles.textInput} ${className}`}
+    ref={inputRef}
     onKeyDown={handlePress}
   />;
 };
