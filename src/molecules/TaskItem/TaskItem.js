d3d541c8f81ab8bc0093bae32b4d629d0325d3ce
maxim-tikhon <tikhon.maxim@gmail.com>
2023-05-18 11:31:15 +0200

    add posibility to toggle task by clicking on task description

--- src/molecules/TaskItem/TaskItem.js
+++ src/molecules/TaskItem/TaskItem.js
@@ -5,10 +5,16 @@ import styles from './TaskItem.module.scss';
 function TaskItem({ task, onDeleteTask, onCheckTask }) {
 	const taskClasses = task.completed ? `${styles.task} ${styles.completed}` : styles.task;
 
+	const toggleTask = () => {
+		onCheckTask({ id: task.id, checked: !task.completed });
+	};
+
 	return (
 		<div>
 			<CheckBoxInput checked={task.completed} onCheck={(checked) => onCheckTask({ id: task.id, checked })} />
-			<span className={taskClasses}>{task.description}</span>
+			<span role="button" tabIndex="0" className={taskClasses} onClick={toggleTask} onKeyDown={toggleTask}>
+				{task.description}
+			</span>
 			<ClearIcon className={styles.deleteIcon} onClick={() => onDeleteTask(task.id)} />
 		</div>
 	);
